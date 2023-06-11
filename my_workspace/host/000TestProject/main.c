a90fb71a5a7a6aa71e0ffbad516911bc7c3b36ce
jesberpen <66469506+jesberpen@users.noreply.github.com>
2023-06-11 10:41:53 +0100

    First setup

---
+++ my_workspace/host/000TestProject/main.c
@@ -0,0 +1,16 @@
+/*
+ * main.c
+ *
+ *  Created on: 11 Jun 2023
+ *      Author: jberjano
+ */
+
+#include<stdio.h>
+
+int main(void)
+{
+	printf("Hello world\n");
+	printf("Press 'Enter' to exit this application\n");
+	getchar();
+	return 0;
+}
